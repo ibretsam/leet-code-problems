37ebebd9cb17eee92d5188801ea5f7c1de21d41e
Khanh Le <khanhle.3109@gmail.com>
2024-07-23 14:15:53 +0700

    1. Two Sum

--- twoSum.py
+++ twoSum.py
@@ -1,41 +1,45 @@
 """
-167. Two Sum II - Input Array Is Sorted
-Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
-find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] 
-and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
-
-Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
-The tests are generated such that there is exactly one solution. You may not use the same element twice.
-Your solution must use only constant extra space.
+1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers 
+such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the 
+same element twice.
+You can return the answer in any order.
 
 Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-Input: numbers = [2,7,11,15], target = 9
-Output: [1,2]
-Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
 Example 2:
-
-Input: numbers = [2,3,4], target = 6
-Output: [1,3]
-Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
-Example 3:
-
-Input: numbers = [-1,0], target = -1
+Input: nums = [3,2,4], target = 6
 Output: [1,2]
-Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
  
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 """
 
-def twoSum(numbers, target):
-    l, r = 0, len(numbers) - 1
-    while l < r:
-        if numbers[l] + numbers[r] == target:
-            return [l + 1, r + 1]
-        elif numbers[l] + numbers[r] > target:
-            r -= 1
-        else:
-            l += 1
+def twoSum(nums, target):
+    hm = {}
+    for i, num in enumerate(nums):
+        if target - num in hm:
+            return [hm[target - num], i]        
+        hm[num] = i
+        
+
+
 
 print(twoSum([2,7,11,15], 9))
-print(twoSum([2,3,4], 6))
-print(twoSum([-1,0], -1))
+print(twoSum([3,2,4], 6))
+print(twoSum([3,3], 6))
