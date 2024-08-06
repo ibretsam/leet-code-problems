aefa29abcc3fdf477675718ea38cf2f9f7f3f117
Khanh Le <masterbruculous@gmail.com>
2024-08-06 22:39:50 +0700

    224. Basic Calculator

--- calculate.py
+++ calculate.py
@@ -17,7 +17,7 @@ Output: 23
  
 
 Constraints:
-1 <= s.length <= 3 * 105
+1 <= s.length <= 3 * 10^5
 s consists of digits, '+', '-', '(', ')', and ' '.
 s represents a valid expression.
 '+' is not used as a unary operation (i.e., "+1" and "+(2 + 3)" is invalid).
@@ -27,27 +27,34 @@ Every number and running calculation will fit in a signed 32-bit integer.
 """
 
 def calculate(s):
-    stack = []
-    operators = ["+", "-"]
-    curr_operators = []
-    for i, c in enumerate(s):
-        if c.isnumeric():
-            stack.append(int(c))
-            if curr_operators:
-                operator = curr_operators.pop()
-                num1, num2 = stack.pop(), stack.pop()
-                if operator == "+":
-                    stack.append(num1 + num2)
-                else:
-                    stack.append(num2 - num1)
-        elif c in operators:
-            curr_operators.append(c)
-    s = [str(i) for i in stack]
-    return int("".join(s))
+    sign, sum, stack = 1, 0, []
+    i = 0
+    while i < len(s):
+        if s[i].isdigit():
+            num = []
+            while i < len(s) and s[i].isdigit():
+                num.append(s[i])
+                i += 1
+            sum += int(''.join(num)) * sign
+            i -= 1
+        elif s[i] == '+':
+            sign = 1
+        elif s[i] == '-':
+            sign = -1
+        elif s[i] == '(':
+            stack.append(sum)
+            stack.append(sign)
+            sum = 0
+            sign = 1
+        elif s[i] == ')':
+            sum = stack.pop() * sum
+            sum += stack.pop()
+        i += 1
+    return sum
 
-
-# print(calculate("1 + 1")) # 2
-# print(calculate(" 2-1 + 2 ")) # 3
-# print(calculate("(1+(4+5+2)-3)+(6+8)")) # 23
-# print(calculate("2147483647")) # 2147483647
+print(calculate("1 + 1")) # 2
+print(calculate(" 2-1 + 2 ")) # 3
+print(calculate("(1+(4+5+2)-3)+(6+8)")) # 23
+print(calculate("2147483647")) # 2147483647
 print(calculate("1-(     -2)")) # 3
+print(calculate("2147483647"))
