c9eba501032551a02c73f164ab0f023f71fecc54
Khanh Le <masterbruculous@gmail.com>
2024-08-27 23:15:35 +0700

    146. LRU Cache

---
+++ LRUCache.py
@@ -0,0 +1,101 @@
+"""
+146. LRU Cache
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the 
+key-value pair to the cache. If the number of keys exceeds the capacity from this operation, 
+evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+Example 1:
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+Explanation
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+lRUCache.get(1);    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+lRUCache.get(2);    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+lRUCache.get(1);    // return -1 (not found)
+lRUCache.get(3);    // return 3
+lRUCache.get(4);    // return 4
+ 
+
+Constraints:
+1 <= capacity <= 3000
+0 <= key <= 10^4
+0 <= value <= 10^5
+At most 2 * 10^5 calls will be made to get and put.
+"""
+
+class ListNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = ListNode(0, 0)
+        self.tail = ListNode(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def _insert(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+
+    def _remove(self, node):
+        prev, next = node.prev, node.next
+        prev.next = next
+        next.prev = prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._insert(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = ListNode(key, value)
+        self._insert(node)
+        self.cache[key] = node
+
+        if len(self.cache) > self.capacity:
+            lru = self.head.next
+            self._remove(lru)
+            del self.cache[lru.key]
+
+
+# Test cases
+lRUCache = LRUCache(2)
+lRUCache.put(1, 1) # cache is {1=1}
+lRUCache.put(2, 2) # cache is {1=1, 2=2}
+print(lRUCache.get(1)) # 1
+lRUCache.put(3, 3) # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+print(lRUCache.get(2)) # -1
+lRUCache.put(4, 4) # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+print(lRUCache.get(1)) # -1
+print(lRUCache.get(3)) # 3
+print(lRUCache.get(4)) # 4
