8c55b61c0bcaa931bfbc0f938ad7b7dfea222728
Khanh Le <masterbruculous@gmail.com>
2024-09-08 14:12:48 +0700

    33. Search in Rotated Sorted Array

--- search.py
+++ search.py
@@ -1,18 +1,63 @@
-def search(nums, target):
-    if not nums:
-            return -1
-    mid = len(nums) // 2
-    if target == nums[mid]:
-        return mid
-    elif target < nums[mid]:
-        return search(nums[:mid], target)
-    else:
-        right_search = search(nums[mid + 1:], target)
-        if right_search == -1:
-            return -1
-        else:
-            return right_search + mid + 1
-        
+"""
+33. Search in Rotated Sorted Array
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) 
+such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, 
+or -1 if it is not in nums.
+You must write an algorithm with O(log n) runtime complexity.
 
-nums, target = [-1,0,3,5,9,12,15,17,19,22,25,30,50], 12
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+[4, 5, 6, 7, 8, 9, 0, 1, 2], target = 0
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+Example 3:
+Input: nums = [1], target = 0
+Output: -1
+
+Constraints:
+1 <= nums.length <= 5000
+-10^4 <= nums[i] <= 10^4
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-10^4 <= target <= 10^4
+"""
+
+def search(nums, target):
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = (l + r) // 2
+
+        if target == nums[m]:
+            return m
+        
+        if nums[l] <= nums[m]:
+            if nums[l] <= target < nums[m]:
+                r = m - 1
+            else:
+                l = m + 1
+        else:
+            if nums[m] < target <= nums[r]:
+                l = m + 1
+            else:
+                r = m - 1
+    return -1
+# Test cases
+# nums, target = [4, 5, 6, 7, 8, 9, 0, 1, 2], 0
+# print(search(nums, target)) # 6
+
+# nums, target = [4,5,6,7,0,1,2], 0
+# print(search(nums, target)) # 4
+
+# nums, target = [4,5,6,7,0,1,2], 3
+# print(search(nums, target))
+
+nums, target = [5, 1, 3], 0
 print(search(nums, target))
