6fb46c47fb4f8dfc48d44ae7dcb37d653b18b0d8
Khanh Le <masterbruculous@gmail.com>
2024-12-21 00:23:26 +0700

    10. Regular Expression Matching

---
+++ isMatch.py
@@ -0,0 +1,48 @@
+"""
+10. Regular Expression Matching
+Given an input string s and a pattern p, implement regular expression matching with support 
+for '.' and '*' where:
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Example 1:
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' 
+once, it becomes "aa".
+
+Example 3:
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+ 
+Constraints:
+1 <= s.length <= 20
+1 <= p.length <= 20
+s contains only lowercase English letters.
+p contains only lowercase English letters, '.', and '*'.
+It is guaranteed for each appearance of the character '*', there will be a previous valid 
+character to match.
+"""
+def isMatch(s: str, p: str) -> bool:
+    m, n = len(s), len(p)
+    def dfs(i, j):
+        if j == n:
+            return i == m
+        match = i < m and (s[i] == p[j] or p[j] == ".")
+        if j + 1 < n and p[j + 1] == "*":
+            return dfs(i, j + 2) or (match and dfs(i + 1, j))
+        if match:
+            return dfs(i + 1, j + 1)
+        return False
+    return dfs(0, 0)
+
+s = "aaa"
+p = "ab*a*c*a"
+print(isMatch(s, p)) # True
